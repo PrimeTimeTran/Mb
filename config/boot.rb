b239ac468ac6cf313e41a5fe2e3aa9bd62e4f94b
Loi Tran <lvt15@my.fsu.edu>
2016-07-25 15:43:00 +0700

    Going live

--- config/boot.rb
+++ config/boot.rb
@@ -3,12 +3,3 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 require 'rails/commands/server'
-
-module Rails
-  class Server
-    super :default_options_alias :default_options
-    def default_options
-      default_options_alias.merge!(:Port => 3333)
-    end
-  end
-end
